6d0dda53bbac6856572b7082c85c1406ecfcec6e
Nilaydnm <nilaydonmez390@gmail.com>
2025-07-24 17:12:40 +0300

    Business katmanına interfaces ve managers(uygulama) eklendi.

---
+++ Business/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entities;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User> GetByUsernameAsync(string username);
+        Task<User> GetByIdAsync(int id);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+        Task<List<User>> GetAllAsync();
+    }
+}
---
+++ Business/Managers/UserManager.cs
@@ -0,0 +1,55 @@
+﻿using Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataAccess.Interfaces;
+using Entities;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Managers
+{
+    public class UserManager : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserManager(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> GetByUsernameAsync(string username)
+        {
+            return await _userRepository.GetByUsernameAsync(username);
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _userRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _userRepository.AddAsync(user);
+            await _userRepository.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _userRepository.Update(user);
+            await _userRepository.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            _userRepository.Remove(user);
+            await _userRepository.SaveChangesAsync();
+        }
+
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await _userRepository.GetAllAsync();
+        }
+    }
+}
+
